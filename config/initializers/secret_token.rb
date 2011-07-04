7f597ad917fdbaf58c2a54bec236f2271a2a99af
Ivan Osadchii <ivan.osadchii@gmail.com>
2011-07-04 11:05:38 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RackCacheTest::Application.config.secret_token = 'b874d129a3dad143a8c3c388059c060afa2950e70d8212c9bc0c1feba9b7d79fc2d2a9b0c61cdd6652f80bc1c6e15cd8bd6599aa0e97dae6d05e15be5ec529cd'
